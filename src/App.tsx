54969b17720fa06d15cc9b9492dded315d6e46a3
mistercouscous <erwan.baillon@epitech.eu>
2023-02-09 18:19:34 +0100

    UPDATE simplification of city lists and api operation

--- src/App.tsx
+++ src/App.tsx
@@ -1,145 +1,15 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
-import axios from 'axios';
-
-interface WeatherState {
-  temp: number;
-  city: string;
-}
-
-interface CityState {
-  insee: string;
-  ncity: string;
-}
-
-const API_KEY = 'YOUR_API_KEY';
-
-async function getCity(city : string) : Promise<CityState> {
-  const response = await axios.get(`https://api.meteo-concept.com/api/location/cities?token=${API_KEY}&search=${city}`);
-  const data = response.data;
-  const insee = data.cities[0].insee;
-  const ncity = city;
-  return {insee, ncity};
-}
-
-async function getWeather(city : string, insee: string): Promise<WeatherState> {
-  const response = await axios.get(`https://api.meteo-concept.com/api/forecast/nextHours?token=${API_KEY}&insee=${insee}`);
-  const data = response.data;
-  const temp = data.forecast[0].temp2m;
-  return {temp, city};
-};
+import { Weather } from './weather/Weather';
 
 function App() {
-  const [weather, setWeather] = useState<WeatherState>({temp: 0, city: 'NONE'});
-  const [city, setCity] = useState<string>('Select your city');
-  const [oldCity, setOldCity] = useState<string>('Select your city');
-  useEffect(() => {
-    setCity(city);
-
-    if (oldCity != city) {
-      setOldCity(city);
-    } else if (city === 'Select your city') {
-      return;
-    } else {
-      return;
-    }
-    getCity(city).then((result) => {
-      getWeather(city, result.insee).then((weather) => {
-        setCity(result.ncity);
-        setWeather(weather);
-      }).catch((error) => {
-        console.log(error);
-      });
-    }).catch((error) => {
-      console.log(error);
-      setWeather({temp: 0, city: city});
-    });
-  }, [city, weather]);
-
-  useEffect(() => {
-    setInterval(() => {
-      getCity(city).then((result) => {
-        getWeather(city, result.insee).then((weather) => {
-          setCity(result.ncity);
-          setWeather(weather);
-        }).catch((error) => {
-          console.log(error);
-        });
-      }).catch((error) => {
-        console.log(error);
-      });
-    }, 3600000);
-  }, [weather]);
 
   return (
     <div className="App">
       <div>
         <h1>StormyDay App</h1>
       </div>
-      <div className='my_select_city'>
-        <select className='selector_city' value={city} onChange={(e) => setCity(e.target.value)}>
-          <option value="Select your city">Select your city</option>
-          <option value="Bordeaux">Bordeaux</option>
-          <option value="Paris">Paris</option>
-          <option value="Lyon">Lyon</option>
-          <option value="Marseille">Marseille</option>
-          <option value="Toulouse">Toulouse</option>
-          <option value="Nice">Nice</option>
-          <option value="Nantes">Nantes</option>
-          <option value="Montpellier">Montpellier</option>
-          <option value="Strasbourg">Strasbourg</option>
-          <option value="Brest">Brest</option>
-          <option value="Rennes">Rennes</option>
-          <option value="Le Havre">Le Havre</option>
-          <option value="Reims">Reims</option>
-          <option value="Lille">Lille</option>
-          <option value="Saint-Étienne">Saint-Étienne</option>
-          <option value="Toulon">Toulon</option>
-          <option value="Grenoble">Grenoble</option>
-          <option value="Dijon">Dijon</option>
-          <option value="Angers">Angers</option>
-          <option value="Nîmes">Nîmes</option>
-          <option value="Villeurbanne">Villeurbanne</option>
-          <option value="Le Mans">Le Mans</option>
-          <option value="Aix-en-Provence">Aix-en-Provence</option>
-          <option value="Boulogne-Billancourt">Boulogne-Billancourt</option>
-          <option value="Limoges">Limoges</option>
-          <option value="Clermont-Ferrand">Clermont-Ferrand</option>
-          <option value="Tours">Tours</option>
-          <option value="Amiens">Amiens</option>
-          <option value="Perpignan">Perpignan</option>
-          <option value="Besançon">Besançon</option>
-          <option value="Metz">Metz</option>
-          <option value="Rouen">Rouen</option>
-          <option value="Argenteuil">Argenteuil</option>
-          <option value="Nancy">Nancy</option>
-          <option value="Saint-Denis">Saint-Denis</option>
-          <option value="Montreuil">Montreuil</option>
-          <option value="Caen">Caen</option>
-          <option value="Nanterre">Nanterre</option>
-          <option value="Roubaix">Roubaix</option>
-          <option value="Mulhouse">Mulhouse</option>
-          <option value="Saint-Paul">Saint-Paul</option>
-          <option value="Poitiers">Poitiers</option>
-          <option value="Orléans">Orléans</option>
-          <option value="Rueil-Malmaison">Rueil-Malmaison</option>
-          <option value="Avignon">Avignon</option>
-          <option value="Tourcoing">Tourcoing</option>
-          <option value="Versailles">Versailles</option>
-          <option value="Créteil">Créteil</option>
-          <option value="Colombes">Colombes</option>
-          <option value="Aulnay-sous-Bois">Aulnay-sous-Bois</option>
-          <option value="Bourges">Bourges</option>
-          <option value="Vitry-sur-Seine">Vitry-sur-Seine</option>
-          <option value="Arcachon">Arcachon</option>
-        </select>
-      </div>
-      <p></p>
-      <div className="weather">
-          <p className='output_temp'>
-            {weather.city}: {weather.temp}°C
-          </p>
-      </div>
+      <Weather />
     </div>
   );
 }
---
+++ src/weather/Weather.tsx
@@ -0,0 +1,83 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+interface CityState {
+    insee: string;
+    city: string;
+    temp: number;
+}
+
+const cities = [
+    'Select your city',
+    'Bordeaux',
+    'Paris',
+    'Lyon',
+    'Marseille',
+    'Toulouse'
+]
+const API_KEY = 'YOUR_API_KEY';
+
+async function getCity(city : string) : Promise<CityState> {
+    if (city === 'NONE') {
+        city = 'Bordeaux';
+    }
+    const response = await axios.get(`https://api.meteo-concept.com/api/location/cities?token=${API_KEY}&search=${city}`);
+    if (response === undefined) {
+        return {insee: 'NULL', city: 'NONE', temp: 0};
+    }
+    const data = response.data;
+    const res = await axios.get(`https://api.meteo-concept.com/api/forecast/nextHours?token=${API_KEY}&insee=${data.cities[0].insee}`);
+    if (res === undefined) {
+        return {insee: 'NULL', city: 'NONE', temp: 0};
+    }
+    const dt = res.data;
+    console.log(dt);
+    const insee = data.cities[0].insee;
+    const temp = dt.forecast[0].temp2m;
+    return {insee, city, temp};
+    }
+
+export function Weather() {
+    const [city, setCity] = useState<CityState>({insee: 'NULL', city: 'NONE', temp: 0});
+    const [oldCity, setOldCity] = useState<string>('Select your city');
+
+    useEffect(() => {
+        if (oldCity !== 'Select your city') {
+            getCity(oldCity).then((city) => setCity(city));
+        }
+    }, [oldCity]);
+
+    useEffect(() => {
+        if (city.city === oldCity) {
+            return;
+        } else {
+            setCity(city);
+        }
+        setInterval(() => {
+            getCity(city.city).then((city) => setCity(city));
+        }, 3600000);
+    }, [city, oldCity]);
+    
+    return (
+        <div className="Weather">
+          <div className='my_select_city'>
+          <select className='selector_city' value={oldCity} onChange={(e) => setOldCity(e.target.value)}>
+            {
+                cities.map((city, index) => {
+                    return <option key={index} value={city}>{city}</option>
+                })
+            }
+          </select>
+        </div>
+        <div className="weather">
+            <p className='output_temp'>
+              {
+                city.city === 'NONE' ? 'Select your city' : city.city
+              }: {
+                city.temp === 0 ? 'Loading...' : city.temp
+              }°C
+            </p>
+        </div>
+        </div>
+    );
+}
